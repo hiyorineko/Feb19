98f55503edf404c0c42ddc05d01896fda97dbcc9
hiyoriaya <hiyoriaya@gmail.com>
2016-02-19 11:06:31 +0900

    first commit

---
+++ gaibu.js
@@ -0,0 +1,8 @@
+document.write("Hello Java!……Script"+"<br>");
+function fncCall(){
+	var langs = new Array("Java","PHP","JavaScript");
+	for(var i=0;i<langs.length;i++){
+		document.write(langs[i]+"<br>");
+	}
+	alert("こんにちはJava！！！！");
+}
